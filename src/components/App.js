8a74fb3af8fe7032d778cf9972f81a236f60697d
shwe yee <yonngelay@gmail.com>
2019-07-05 17:28:29 +0800

    Showing the list of songs

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { selectSong } from '../actions';
+
+import SongList from './SongList';
+
+function App() {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
